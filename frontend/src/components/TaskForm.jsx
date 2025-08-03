b0e754bdf9dc4738635ddaf21ffb38e965c8f69a
Miorena <miorenafinaritra@gmail.com>
2025-08-03 03:59:16 +0300

    Add modify option

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -15,9 +15,7 @@ function TaskForm({onAdd}) {
         try {
             const response = await fetch(`http://127.0.0.1:8000/api/tasks/`, {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
+                headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({title, is_completed: false})
             });
 
--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -8,9 +8,7 @@ function TaskItem({ task, onUpdate, onDelete }) {
         try {
             const response = await fetch(`http://127.0.0.1:8000/api/tasks/${task.id}/`, {
                 method: "PATCH",
-                headers: {
-                "Content-Type": "application/json"
-                },
+                headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({ is_completed: !task.is_completed })
             });
 
